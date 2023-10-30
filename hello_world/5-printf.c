80569ed1a22fdd661a220c63b1cdbb128fbece05
Micke2310 <maikyarachan@gmail.com>
2023-10-30 12:56:21 -0700

    up

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/*
+ *main - Printe a statemet with proper grammar
+ *fallowed by a new line and return 0. 
+ * 
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with propeer gramar, but the outcome is a place of art,\n");
+	return (0);
+}
