8a2a738e59984a752385d7e3816ecc9826493e92
Micke2310 <maikyarachan@gmail.com>
2023-10-30 12:21:10 -0700

    up

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
-int main(void){
-	puts ("\"Programming is like building a multilingual puzzle");
-        return (0); }
+/*main 
+ * return
+ * */
+int main(void)
+             {  
+	        puts ("\"Programming is like building a multilingual puzzle");
+                   return (0); 
+	     }
