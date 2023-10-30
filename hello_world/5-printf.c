4294f0eb5f39e29102fdde316c9cbe817f5b2646
Micke2310 <maikyarachan@gmail.com>
2023-10-30 13:08:42 -0700

    up

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /*
  *main - Printe a statemet with proper grammar
- *fallowed by a new line and return 0. 
- * 
+ *fallowed by a new line and return 0.
+ *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with propeer gramar, but the outcome is a place of art,\n");
+	printf("with proper grammar, but the outcome is a piace of art,\n");
 	return (0);
 }
